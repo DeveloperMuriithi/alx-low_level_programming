745a7b43e31ec1769df2a623f0bc7fc59812ead7
DeveloperMuriithi <developermuriithi@gmail.com>
2024-02-26 23:18:48 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisd.h>
+/**
+ *main - a program that prints without using printf or puts
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stfout);
+return (1);
+}
