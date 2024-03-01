66c66f6df92ad0bb769af7bd857967c1ce910a0f
DeveloperMuriithi <developermuriithi@gmail.com>
2024-03-01 19:45:23 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while
+		(c <= '9') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
